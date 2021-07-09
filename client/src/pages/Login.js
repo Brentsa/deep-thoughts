7ec54ac6eac70a4ec79075ba60ae7a77f0694f2d
Solidshots <seanbrent5@gmail.com>
2021-07-09 08:41:54 -0400

    use mutations from front-end

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+
+  const [login, {error}] = useMutation(LOGIN_USER)
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -16,6 +20,14 @@ const Login = (props) => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    try{
+      const {data} = await login({variables: {...formState}});
+      console.log(data);
+    }
+    catch(e){
+      console.error(e);
+    }
 
     // clear form values
     setFormState({
@@ -53,6 +65,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
