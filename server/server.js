86ec3db5c1c9bd51bba0306580c23e070e9880c1
Solidshots <seanbrent5@gmail.com>
2021-07-11 08:57:47 -0400

    final version

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const {path} = require('path');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
